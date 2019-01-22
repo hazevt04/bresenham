8c76aadba3509b4ab4f883da9a777bccc5836396
Glenn <hazelnutvt04@gmail.com>
2019-01-22 00:23:23 -0500

    Added timer and for loop to main.

--- main.cpp
+++ main.cpp
@@ -4,10 +4,15 @@
 #include "PNGRenderer.h"
 #include "BresenhamLineDrawer.h"
 
+#include <iomanip>
+#include <ctime>
+
+#define NUM_LINES 100
+
 int main( int argc, char* argv[] ) {
    
-   ulong width = 400;
-   ulong height = 400;
+   ulong width = 1000;
+   ulong height = 1000;
    ulong line_color = ImageData::BLACK;
    std::string filename = "temp.png";
    ulong background_color = ImageData::WHITE;
@@ -15,20 +20,41 @@ int main( int argc, char* argv[] ) {
 
    BresenhamLineDrawer* bresenham_line_drawer = new BresenhamLineDrawer( image_data );
 
-   Point* start_point = new Point( 20, 40 ); 
-   Point* end_point = new Point( 100, 100 );
-   Line* line = new Line( start_point, end_point, line_color );
+   Point* start_points[NUM_LINES];
+   Point* end_points[NUM_LINES];
+   Line*  lines[NUM_LINES];
 
-   bresenham_line_drawer->draw( line );
+   std::clock_t start_time;
+   double total_duration = 0.0;
+
+   start_time = std::clock();
+   for ( int index = 0; index < NUM_LINES; index++ ) {
+      ulong x0 = 20 + ( index * 10 );
+      ulong y0 = 40;
+      ulong x1 = 100 + ( index * 10 );
+      ulong y1 = 100;
+      start_points[ index ] = new Point( x0, y0 ); 
+      end_points[ index ] = new Point( x1, y1 );
+      lines[ index ] = new Line( start_points[ index ], end_points[ index ], line_color );
    
+      bresenham_line_drawer->draw( lines[ index ] );
+   }
+   total_duration = ( std::clock() - start_time ) / ( double ) CLOCKS_PER_SEC;
+   std::cout << "Total Duration " << std::setprecision(3) << total_duration 
+      << " seconds." << std::endl;
+
    PNGRenderer* renderer = new PNGRenderer( image_data );
 
    renderer->render( );
 
    delete renderer;
-   delete line;
-   delete end_point;
-   delete start_point;
+   
+   for ( int index = 0; index < NUM_LINES; index++ ) {
+      delete lines[ index ];
+      delete end_points[ index ];
+      delete start_points[ index ];
+   }
+   
    delete image_data;
    return 0;
 }
